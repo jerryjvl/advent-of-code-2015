610e58e5b890351ccf698864a6021c03e987a865
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-08 00:42:16 +1100

    solve: day 3

--- day-3/Program.cs
+++ day-3/Program.cs
@@ -1,6 +1,58 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 
-long total = 0;
+var map = new Dictionary<(long, long), int>();
+long x = 0;
+long y = 0;
+map[(x, y)] = 1;
+foreach (var dir in lines[0])
+{
+    switch (dir)
+    {
+        case '^':
+            y--;
+            break;
+        case 'v':
+            y++;
+            break;
+        case '<':
+            x--;
+            break;
+        case '>':
+            x++;
+            break;
+    }
 
-// Expected: ...
-Console.WriteLine(total);
+    int count = 0;
+    map.TryGetValue((x, y), out count);
+    map[(x, y)] = count + 1;
+}
+Console.WriteLine(map.Count);
+
+map = new Dictionary<(long, long), int>();
+long sx = 0, sy = 0, rx = 0, ry = 0;
+map[(0, 0)] = 2;
+foreach (var dir in lines[0])
+{
+    switch (dir)
+    {
+        case '^':
+            sy--;
+            break;
+        case 'v':
+            sy++;
+            break;
+        case '<':
+            sx--;
+            break;
+        case '>':
+            sx++;
+            break;
+    }
+
+    int count = 0;
+    map.TryGetValue((sx, sy), out count);
+    map[(sx, sy)] = count + 1;
+
+    (sx, sy, rx, ry) = (rx, ry, sx, sy);
+}
+Console.WriteLine(map.Count);
