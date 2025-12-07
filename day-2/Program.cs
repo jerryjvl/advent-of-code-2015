e60bd5ffd0f5126650cee7f695c714b70bd3b546
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-08 00:30:26 +1100

    solve: day 2

--- day-2/Program.cs
+++ day-2/Program.cs
@@ -1,6 +1,20 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿using System.Collections.Immutable;
 
-long total = 0;
+var lines = File.ReadAllLines("input.txt");
 
-// Expected: ...
-Console.WriteLine(total);
+var packages = lines.Select(line =>
+    line.Split('x').Select(num => long.Parse(num)).Order().ToArray());
+
+long paper = 0;
+long ribbon = 0;
+foreach (var package in packages)
+{
+    long l = package[0], w = package[1], h = package[2];
+    long[] s = [l*w, w*h, h*l];
+    paper += s[0] * 3 + s[1] * 2 + s[2] * 2;
+
+    ribbon += (l + w) * 2 + (l * w * h);
+}
+
+Console.WriteLine(paper);
+Console.WriteLine(ribbon);
