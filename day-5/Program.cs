3067f513b12c09e643aa5b83ff011fcea623a67a
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-08 01:12:48 +1100

    solve: day 5

--- day-5/Program.cs
+++ day-5/Program.cs
@@ -1,6 +1,25 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 
 long total = 0;
+foreach (var word in lines)
+{
+    if (word.Count(ch => "aeiou".Contains(ch)) < 3) continue;
+    if (word.SkipLast(1).Zip(word.Skip(1)).All(pair => pair.First != pair.Second)) continue;
+    if (((string[])["ab", "cd", "pq", "xy"]).Any(word.Contains)) continue;
 
-// Expected: ...
+    total++;
+}
+Console.WriteLine(total);
+
+total = 0;
+foreach (var word in lines)
+{
+    bool skip = true;
+    for (int ix = 0; ix < word.Length - 2; ix++)
+        skip &= word.IndexOf(word.Substring(ix, 2), ix + 2) == -1;
+    if (skip) continue;
+    if (word.SkipLast(2).Zip(word.Skip(2)).All(pair => pair.First != pair.Second)) continue;
+
+    total++;
+}
 Console.WriteLine(total);
