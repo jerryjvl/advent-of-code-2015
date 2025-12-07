acb76e15c761820b5164c1031bed3d550c9bbb74
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-07 23:59:03 +1100

    solve: day 1

--- day-1/Program.cs
+++ day-1/Program.cs
@@ -1,6 +1,17 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 
-long total = 0;
+// Find the floor
+Console.WriteLine(lines[0].Select(ch => ch == '(' ? 1 : -1).Sum());
 
-// Expected: ...
-Console.WriteLine(total);
+// Find the index (+1) where we go into the basement
+var sum = 0;
+var deltas = lines[0].Select(ch => ch == '(' ? 1 : -1).ToArray();
+for (int ix = 0; ix < deltas.Length; ix++)
+{
+    sum += deltas[ix];
+    if (sum < 0)
+    {
+        Console.WriteLine(ix + 1);
+        break;
+    }
+}
