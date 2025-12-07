1b5d32e49d7c0a407417b8df3ecf27389a53932e
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-08 00:54:02 +1100

    solve: day 4

--- day-4/Program.cs
+++ day-4/Program.cs
@@ -1,6 +1,27 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿using System.Security.Cryptography;
+using System.Text;
 
-long total = 0;
+var lines = File.ReadAllLines("input.txt");
+var prefix = lines[0];
 
-// Expected: ...
-Console.WriteLine(total);
+for (long num = 1; num < long.MaxValue; num++)
+{
+    var value = prefix + num.ToString();
+    var bytes = MD5.HashData(Encoding.UTF8.GetBytes(value));
+    if (bytes[0] == 0 && bytes[1] == 0 && (bytes[2] & 0xF0) == 0)
+    {
+        Console.WriteLine(num);
+        break;
+    }
+}
+
+for (long num = 1; num < long.MaxValue; num++)
+{
+    var value = prefix + num.ToString();
+    var bytes = MD5.HashData(Encoding.UTF8.GetBytes(value));
+    if (bytes[0] == 0 && bytes[1] == 0 && bytes[2]  == 0)
+    {
+        Console.WriteLine(num);
+        break;
+    }
+}
